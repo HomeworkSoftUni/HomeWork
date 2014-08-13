25395be3d3b8966511aefd783cd275fc788be267
iuli1478 <iuli.iuli17@gmail.com>
2014-08-13 15:39:52 +0300

    PHP

---
+++ _01_Semester/_05_PHP/01.HomeworkPHPSyntax/02.SumTwoNumbers/SumTwoNumbers.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>PHP</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+            $firstNumber = 1.567808;
+            $secondNumber = 0.356;
+            $sum = $firstNumber + $secondNumber;
+            echo '$firstNumber + $secondNumber = ' . " $firstNumber + $secondNumber = " . round($sum, 2);
+        ?>
+    </body>
+</html>
