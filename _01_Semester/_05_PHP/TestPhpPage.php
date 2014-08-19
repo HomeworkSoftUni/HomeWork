4564fb61528b46bb378c0ad9781f38992dfccfa6
iuli1478 <iuli.iuli17@gmail.com>
2014-08-19 17:59:41 +0300

    up

---
+++ _01_Semester/_05_PHP/TestPhpPage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+     //   echo phpinfo();
+        $a = 100;
+        for ($i = 1; $i <= $a; $i++) {
+            echo $i . "</br>";
+        }
+        ?>
+    </body>
+</html>
